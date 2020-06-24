6795bddaacfa072af620ec5a33ef36d9dc24bb3a
riantavares <rian.tavares@live.com>
2020-06-24 13:12:22 -0300

    creating search element and styling it

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaSearchengin } from "react-icons/fa";
 
 const Home = () => {
     return (
-        <>
-        HELLO WORLD
-        </>
+        <section class="page-home">
+            <form class="page-home__form" action="">
+                <input type="text" placeholder="Search.." name="search" />
+                <button type="submit"><FaSearchengin size="17"/></button>
+            </form>
+            <section>Itens</section>
+        </section>
     )
 }
 
