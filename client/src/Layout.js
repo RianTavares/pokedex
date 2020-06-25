86d453ec750b66f0b90a973e0285e312e708233f
riantavares <rian.tavares@live.com>
2020-06-24 21:21:12 -0300

    adding Fork me on GitHub ribbon and styling it

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -6,6 +6,12 @@ import Footer from "./components/Footer";
 const Body = props => {
   return (
     <>
+      <a className="ribbon" 
+      href="https://github.com/RianTavares/pokedex" 
+      target="_blank" 
+      rel="noopener noreferrer">
+        Fork me on GitHub
+      </a>
       <main className="page-content">
           <section className="page-content__card">
             {props.children}
