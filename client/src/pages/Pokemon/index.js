c2b6d76ee5150fcf174737842222aa99d639f763
Rian Tavares <rian.tavares@live.com>
2020-06-27 18:00:23 -0300

    adding back home button

--- client/src/pages/Pokemon/index.js
+++ client/src/pages/Pokemon/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 
 import { useQuery } from "@apollo/react-hooks"
+import { FaArrowLeft } from "react-icons/fa"
 import { POKEMON_BY_ID } from "../../helper/gqlQueries"
 
 import pokeTypes from "../../assets/mocks/pokeTypes"
@@ -13,6 +14,7 @@ import Evolution from "../../components/Evolutions"
 const Pokemon = () => {
   window.scrollTo(0, 0)
 
+  const history = useHistory()
   const [cardColor, setCardColor] = useState("#003a70")
 
   const { id } = useParams()
@@ -23,6 +25,10 @@ const Pokemon = () => {
   const type = data?.pokemon?.types[0]
     ? data.pokemon.types[0].toLowerCase()
     : null
+
+  const backHome = () => {
+    history.push("/")
+  }
 
   useEffect(() => {
     setCardColor(pokeTypes[`${type}`] ? pokeTypes[`${type}`] : "#003a70")
@@ -41,125 +47,134 @@ const Pokemon = () => {
       {error ? <p>template de error</p> : ""}
 
       {data ? (
-        <section className="poke-details">
-          <header
-            className="poke-details__header"
-            style={{ backgroundColor: `${cardColor}` }}
+        <>
+          <button
+            type="button"
+            className="page-content__card__back-home"
+            onClick={backHome}
           >
-            <hgroup>
-              <h1>
-                #{data.pokemon.number} {data.pokemon.name}
-              </h1>
-            </hgroup>
-            <div className="poke-details__header__type">
-              {data.pokemon.types.map((pokemonType) => {
-                return <Types key={pokemonType} type={pokemonType} />
-              })}
-            </div>
-          </header>
-          <div className="poke-details__card">
-            <div className="poke-details__card__infos">
-              <div className="poke-details__card__infos__img-container">
-                <div
-                  className="poke-details__card__infos__img-container__img"
-                  style={{ backgroundImage: `url(${data.pokemon.image})` }}
-                />
-              </div>
-              <div className="poke-details__card__infos__personal">
-                <div className="poke-details__card__infos__personal__data">
-                  <div className="poke-details__card__infos__personal__data__height">
-                    <h2>Altura</h2>
-                    <p>Mínima: {data.pokemon.height.minimum}</p>
-                    <p>Máxima: {data.pokemon.height.maximum}</p>
-                  </div>
-                  <div className="poke-details__card__infos__personal__data__weight">
-                    <h2>Peso</h2>
-                    <p>Mínimo: {data.pokemon.weight.minimum}</p>
-                    <p>Máximo: {data.pokemon.weight.maximum}</p>
-                  </div>
-                </div>
-                <div className="poke-details__card__infos__personal__features">
-                  <div className="poke-details__card__infos__personal__features__resistant">
-                    <h2>Resistente:</h2>
-                    <div className="feature-list">
-                      {data.pokemon.resistant.map((resistantType) => {
-                        return (
-                          <Feature key={resistantType} type={resistantType} />
-                        )
-                      })}
-                    </div>
-                  </div>
-                  <div className="poke-details__card__infos__personal__features__weaknesses">
-                    <h2>Fraquezas:</h2>
-                    <div className="feature-list">
-                      {data.pokemon.weaknesses.map((weaknessType) => {
-                        return (
-                          <Feature key={weaknessType} type={weaknessType} />
-                        )
-                      })}
-                    </div>
-                  </div>
-                </div>
-                <div>Classificação: {data.pokemon.classification}</div>
-              </div>
-            </div>
-            <section
-              className="poke-details__card__sub-header"
+            <FaArrowLeft size={25} color="#c52018" /> <p> Voltar a Home</p>
+          </button>
+          <section className="poke-details">
+            <header
+              className="poke-details__header"
               style={{ backgroundColor: `${cardColor}` }}
             >
               <hgroup>
-                <h2>Ataques e danos</h2>
+                <h1>
+                  #{data.pokemon.number} {data.pokemon.name}
+                </h1>
               </hgroup>
-            </section>
-            <section className="poke-details__card__attacks">
-              <div className="poke-details__card__attacks__fast">
-                <h2>Fast</h2>
-                {data.pokemon.attacks.fast.map((attack) => {
-                  return (
-                    <Attacks
-                      key={attack.name}
-                      type={attack.type}
-                      damage={attack.damage}
-                      name={attack.name}
-                    />
-                  )
+              <div className="poke-details__header__type">
+                {data.pokemon.types.map((pokemonType) => {
+                  return <Types key={pokemonType} type={pokemonType} />
                 })}
               </div>
-              <div className="poke-details__card__attacks__special">
-                <h2>Special</h2>
-                {data.pokemon.attacks.special.map((attack) => {
-                  return (
-                    <Attacks
-                      key={attack.name}
-                      type={attack.type}
-                      damage={attack.damage}
-                      name={attack.name}
-                    />
-                  )
-                })}
+            </header>
+            <div className="poke-details__card">
+              <div className="poke-details__card__infos">
+                <div className="poke-details__card__infos__img-container">
+                  <div
+                    className="poke-details__card__infos__img-container__img"
+                    style={{ backgroundImage: `url(${data.pokemon.image})` }}
+                  />
+                </div>
+                <div className="poke-details__card__infos__personal">
+                  <div className="poke-details__card__infos__personal__data">
+                    <div className="poke-details__card__infos__personal__data__height">
+                      <h2>Altura</h2>
+                      <p>Mínima: {data.pokemon.height.minimum}</p>
+                      <p>Máxima: {data.pokemon.height.maximum}</p>
+                    </div>
+                    <div className="poke-details__card__infos__personal__data__weight">
+                      <h2>Peso</h2>
+                      <p>Mínimo: {data.pokemon.weight.minimum}</p>
+                      <p>Máximo: {data.pokemon.weight.maximum}</p>
+                    </div>
+                  </div>
+                  <div className="poke-details__card__infos__personal__features">
+                    <div className="poke-details__card__infos__personal__features__resistant">
+                      <h2>Resistente:</h2>
+                      <div className="feature-list">
+                        {data.pokemon.resistant.map((resistantType) => {
+                          return (
+                            <Feature key={resistantType} type={resistantType} />
+                          )
+                        })}
+                      </div>
+                    </div>
+                    <div className="poke-details__card__infos__personal__features__weaknesses">
+                      <h2>Fraquezas:</h2>
+                      <div className="feature-list">
+                        {data.pokemon.weaknesses.map((weaknessType) => {
+                          return (
+                            <Feature key={weaknessType} type={weaknessType} />
+                          )
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                  <div>Classificação: {data.pokemon.classification}</div>
+                </div>
               </div>
-            </section>
-            {data.pokemon.evolutions ? (
-              <>
-                <section
-                  className="poke-details__card__sub-header"
-                  style={{ backgroundColor: `${cardColor}` }}
-                >
-                  <hgroup>
-                    <h2>Evoluções</h2>
-                  </hgroup>
-                </section>
-                <section className="poke-details__card__evolution">
-                  {data.pokemon?.evolutions?.map((poke) => {
-                    return <Evolution key={poke.id} id={poke.id} />
+              <section
+                className="poke-details__card__sub-header"
+                style={{ backgroundColor: `${cardColor}` }}
+              >
+                <hgroup>
+                  <h2>Ataques e danos</h2>
+                </hgroup>
+              </section>
+              <section className="poke-details__card__attacks">
+                <div className="poke-details__card__attacks__fast">
+                  <h2>Fast</h2>
+                  {data.pokemon.attacks.fast.map((attack) => {
+                    return (
+                      <Attacks
+                        key={attack.name}
+                        type={attack.type}
+                        damage={attack.damage}
+                        name={attack.name}
+                      />
+                    )
                   })}
-                </section>
-              </>
-            ) : (
-              " "
-            )}
-          </div>
-        </section>
+                </div>
+                <div className="poke-details__card__attacks__special">
+                  <h2>Special</h2>
+                  {data.pokemon.attacks.special.map((attack) => {
+                    return (
+                      <Attacks
+                        key={attack.name}
+                        type={attack.type}
+                        damage={attack.damage}
+                        name={attack.name}
+                      />
+                    )
+                  })}
+                </div>
+              </section>
+              {data.pokemon.evolutions ? (
+                <>
+                  <section
+                    className="poke-details__card__sub-header"
+                    style={{ backgroundColor: `${cardColor}` }}
+                  >
+                    <hgroup>
+                      <h2>Evoluções</h2>
+                    </hgroup>
+                  </section>
+                  <section className="poke-details__card__evolution">
+                    {data.pokemon?.evolutions?.map((poke) => {
+                      return <Evolution key={poke.id} id={poke.id} />
+                    })}
+                  </section>
+                </>
+              ) : (
+                " "
+              )}
+            </div>
+          </section>
+        </>
       ) : (
         ""
       )}
