5d5255707f4fe95cbff557c2a9e96b4157fbb32b
Rian Tavares <rian.tavares@live.com>
2020-06-27 01:39:36 -0300

    creating search engine by pokemons name and removing button from search input

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaSearchengin } from 'react-icons/fa';
 import Pokemon from '../../components/Pokemon';
 
@@ -6,9 +6,29 @@ import { POKEMONS } from '../../helper/gqlQueries'
 import { useQuery } from '@apollo/react-hooks';
 
 const Home = () => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
 
   const { loading, error, data } = useQuery(POKEMONS, {
   });
+
+  const findPokemon = (e) => {
+    e.preventDefault();
+    const inputString = e.target.value;
+
+    setFilteredData(
+      pokemons.filter(item => {
+        return item.name.toLowerCase().includes(inputString)
+      })
+    )
+  }
+
+  useEffect(() => {
+    if (data) {
+      setPokemons(data.pokemons);
+      setFilteredData(data.pokemons);
+    }
+  }, [data])
 
   return (
     <>
@@ -23,24 +43,33 @@ const Home = () => {
       {data ? (
         <section className="page-home">
           <form className="page-home__form" action="">
-            <input type="text" placeholder="Search.." name="search" />
-            <button type="submit"><FaSearchengin size="17" /></button>
+            <input 
+              type="text" 
+              placeholder="Procure pelo nome do seu Pokemon..." 
+              name="search" 
+              onChange={findPokemon} 
+            />
           </form>
           <section>
 
 
             <div className="poke-list">
-              {data.pokemons.map((response) => (
-                <Pokemon
-                  key={response.id}
-                  name={response.name}
-                  img={response.image}
-                  id={response.id}
-                  type={response.types[0]}
-                />
-              ))}
+              {filteredData ? (
+                <>
+                  {filteredData.map((response) => (
+                    <Pokemon
+                      key={response.id}
+                      name={response.name}
+                      img={response.image}
+                      id={response.id}
+                      type={response.types[0]}
+                    />
+                  ))}
+                </>
+              ) : (
+                  ' '
+                )}
             </div>
-
           </section>
         </section>
       ) : (
