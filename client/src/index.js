737e07db8c32348383bc730f960548cacd6f39c2
@RianTavaresOn <rian.tavares@live.com>
2020-07-04 01:19:42 -0300

    removing unnecessary comments

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,7 @@ import React from "react"
 
 import { ApolloProvider } from "@apollo/react-hooks"
 import { client } from "./services/apollo/apollo-connect"
-// import { InMemoryCache } from 'apollo-cache-inmemory'
-// import ApolloClient from "apollo-boost"
 import App from "./App"
-
-// const client = new ApolloClient({
-//   uri: "https://graphql-pokemon.now.sh/",
-//   cache: new InMemoryCache()
-// })
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(
